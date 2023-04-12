f525865dab27549ab51ca42272c86266cdd9b98d
Alexey Saprunov <workernight@yandex.ru>
2023-04-12 17:01:41 +0400

    chore: add some commands

--- handler/commands.cpp
+++ handler/commands.cpp
@@ -5,3 +5,25 @@ BaseCommand::BaseCommand() {}
 void BaseCommand::doCommand(std::string command) {}
 
 void BaseCommand::doCommand() {}
+
+BeginBlockCommand::BeginBlockCommand()
+    : BaseCommand()
+{}
+
+void BeginBlockCommand::doCommand() {}
+
+EndBlockCommand::EndBlockCommand()
+    : BaseCommand()
+{}
+
+void EndBlockCommand::doCommand() {}
+
+EofCommand::EofCommand()
+    : BaseCommand()
+{}
+
+void EofCommand::doCommand() {}
+
+StoreDataCommand::StoreDataCommand()
+    : BaseCommand()
+{}
--- handler/commands.h
+++ handler/commands.h
@@ -1,8 +1,8 @@
 #ifndef COMMANDS_H
 #define COMMANDS_H
 
-#include <string>
 #include "../core/ifaces.h"
+#include <string>
 
 enum COMMAND
 {
@@ -23,6 +23,42 @@ public:
     void doCommand(std::string command) override;
 
     void doCommand() override;
+};
+
+class BeginBlockCommand : public BaseCommand
+{
+public:
+    BeginBlockCommand();
+    virtual ~BeginBlockCommand() = default;
+
+    void doCommand() override;
+};
+
+class EndBlockCommand : public BaseCommand
+{
+public:
+    EndBlockCommand();
+    virtual ~EndBlockCommand() = default;
+
+    void doCommand() override;
+};
+
+class EofCommand : public BaseCommand
+{
+public:
+    EofCommand();
+    virtual ~EofCommand() = default;
+
+    void doCommand() override;
+};
+
+class StoreDataCommand : public BaseCommand
+{
+public:
+    StoreDataCommand();
+    virtual ~StoreDataCommand = defult;
+
+    void doCommand();
 };
 
 #endif
